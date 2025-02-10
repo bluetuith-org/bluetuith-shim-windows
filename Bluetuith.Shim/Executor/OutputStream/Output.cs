f1ec925f74d351c35bc39c4d4c9d82607811482e
darkhz <kmachanwenw@gmail.com>
2025-02-10 21:24:24 +0530

    Bluetuith.Shim: Upgrade and introduce new features
    With this commit, the application will require administrator privileges
    to function (except for 'server' commands, which will automatically
    elevate privileges)
    
    This is a squashed commit which consists of the following changes:
    
    New:
    - RPC server mode, with a system tray icon controller
      and start/stop commands.
    
    - System watcher which watches for changes from the adapter
      and devices (including device battery status)
    
    - Connection methods for Bluetooth Classic Audio profiles
    
    Improved:
    - Pairing and device discovery
    - Object push
    
    Applies various bug fixes as well.

---
+++ Bluetuith.Shim/Executor/OutputStream/Output.cs
@@ -0,0 +1,91 @@
+﻿using Bluetuith.Shim.Executor.Operations;
+using Bluetuith.Shim.Stack.Events;
+using Bluetuith.Shim.Types;
+
+namespace Bluetuith.Shim.Executor.OutputStream;
+
+internal static class Output
+{
+    private static OutputBase _output = new CommandOutput();
+    internal static bool IsOnSocket = false;
+
+    internal static ErrorData StartSocketServer(
+        string socketPath,
+        CancellationTokenSource waitForResume,
+        OperationToken token
+    )
+    {
+        try
+        {
+            _output = new SocketOutput(socketPath, waitForResume, token);
+            IsOnSocket = true;
+
+            _output.WaitForClose();
+        }
+        catch (Exception e)
+        {
+            return Errors.ErrorOperationCancelled.WrapError(new() { { "exception", e.Message } });
+        }
+
+        return Errors.ErrorNone;
+    }
+
+    internal static int Result<T>(T result, ErrorData error, OperationToken token)
+        where T : IResult
+    {
+        if (error != Errors.ErrorNone)
+        {
+            return Error(error, token);
+        }
+
+        IResult outputResult = result;
+        return _output.EmitResult(outputResult, token);
+    }
+
+    internal static int Error(ErrorData error, OperationToken token)
+    {
+        return _output.EmitError(error, token);
+    }
+
+    internal static void Event<T>(T ev, OperationToken token)
+        where T : IEvent
+    {
+        _output.EmitEvent(ev, token);
+    }
+
+    internal static bool ConfirmAuthentication<T>(T authEvent, OperationToken token)
+        where T : AuthenticationEvent
+    {
+        Task.Run(() => _output.EmitAuthenticationRequest(authEvent, token));
+        return authEvent.WaitForResponse();
+    }
+
+    internal static void ReplyToAuthenticationRequest(long operationId, string response)
+    {
+        Task.Run(() => _output.SetAuthenticationResponse(operationId, response));
+    }
+}
+
+internal abstract class OutputBase
+{
+    internal virtual int EmitResult<T>(T result, OperationToken token)
+        where T : IResult
+    {
+        return Errors.ErrorNone.Code.Value;
+    }
+
+    internal virtual int EmitError(ErrorData error, OperationToken token)
+    {
+        return error.Code.Value;
+    }
+
+    internal virtual void EmitEvent<T>(T ev, OperationToken token)
+        where T : IEvent { }
+
+    internal virtual void EmitAuthenticationRequest<T>(T authEvent, OperationToken token)
+        where T : AuthenticationEvent { }
+
+    internal virtual void SetAuthenticationResponse(long operationId, string response) { }
+
+    internal virtual void WaitForClose() { }
+}
